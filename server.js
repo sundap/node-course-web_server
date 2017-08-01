60e84e5f527cb5700bae184058d30eb5b8628c60
Prasanna <prasanna.sundaram@gmail.com>
2017-08-01 20:11:56 +1200

    Projects Page

--- server.js
+++ server.js
@@ -50,7 +50,14 @@ app.get('/',(req,res) => {
 
 app.get('/about',(req,res) => {
   res.render('about.hbs',{
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects',(req,res) => {
+  res.render('projects.hbs',{
+   pageTitle: 'Projects Page',
+   message: 'Portfolio message'
   });
 });
 
